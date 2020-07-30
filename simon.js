a54967286110437e4f57b0a31ab2f86203c830ba
GILEXX <gilbertosa099@outlook.com>
2020-07-30 14:27:53 -0500

    Iluminar secuencia

--- simon.js
+++ simon.js
@@ -8,15 +8,49 @@ class Juego{
   constructor(){
     this.inicializar();
     this.genSecuencia();
+    this.siguienteNivel();
   }
   inicializar(){
     btnEmpezar.classList.add('hide');
-    this.nivel =1;
+    this.nivel = 5;
     this.colores={celeste,violeta,naranja,verde
     }
   }
   genSecuencia(){
     this.secuencia= new Array(10).fill(0).map(n=> Math.floor(Math.random()*4))
+  }
+
+  siguienteNivel(){
+    this.iluminar();
+  }
+
+  numeroColor(num){
+    switch(num){
+      case 0:
+        return 'celeste';
+      case 1:
+        return  'violeta';
+      case 2:
+        return 'naranja';
+      case 3: 
+        return 'verde'  ;   
+    }
+  }
+
+  iluminar(){
+    for (let i=0;i<this.nivel;i++){
+      const color = this.numeroColor(this.secuencia[i])
+      setTimeout(()=>this.iluminarColor(color),1000 * i);
+    }
+  }
+
+  iluminarColor(color){
+    this.colores[color].classList.add('light')
+    setTimeout(()=>this.apagarColor(color), 500)
+  }
+
+  apagarColor(color){
+    this.colores[color].classList.remove('light');
   }
 }
 
