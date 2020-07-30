825fce68ca7ca71bcb799a1c254c943963e50506
GILEXX <gilbertosa099@outlook.com>
2020-07-30 13:05:55 -0500

    modificacion de js

---
+++ simon.js
@@ -0,0 +1,28 @@
+const celeste =document.getElementById('celeste');
+const violeta =document.getElementById('violeta');
+const naranja =document.getElementById('naranja');
+const verde =document.getElementById('verde');
+const btnEmpezar = document.getElementById('btnEmpezar');
+
+class Juego{
+  constructor(){
+    this.inicializar();
+    this.genSecuencia();
+  }
+  inicializar(){
+    btnEmpezar.classList.add('hide');
+    this.nivel =1;
+    this.colores={celeste,violeta,naranja,verde
+    }
+  }
+  genSecuencia(){
+    this.secuencia= new Array(10).fill(0).map(n=> Math.floor(Math.random()*4))
+  }
+}
+
+
+
+
+function empezarJuego(){
+  window.juego = new Juego()
+}
